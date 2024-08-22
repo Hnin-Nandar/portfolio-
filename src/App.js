66bd2a468eb917a8aa4a2aeb1c8aeb0b63d6a2a0
Shine <thuriyac4@gmail.com>
2024-08-22 20:58:42 +0700

    bla bla

--- src/App.js
+++ src/App.js
@@ -3,23 +3,11 @@ import './App.css'
 import Home from "./pages/home";
 
 const App = () => {
+  const name = "Mg Mg";
   return (
-    <div className="text">
-      <h2>Development</h2>
-      <p>
-        I specialize in developing websites using Webflow, a powerful platform that allows for easy customization and responsive design. I have experience building websites for a variety of industries, from small businesses to large corporations. My focus is always on creating functional and smooth websites that are accessible and generate traffic. Webflow Development
-      </p>
-      <Button />
+    <div style={{fontWeight: "bold", fontSize: 30}} >
+      {name}
     </div>
-  )
-}
-
-const Button = ({ }) => {
-  return (
-    <>
-      <button><a href="#">Webflow</a></button>
-      <button><a href="#">Development</a></button>
-    </>
   )
 }
 
