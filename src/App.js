45e2e9e9d2ce09d626006d6a2839ca13fca1620d
Hnin-Nandar <hninnandarkhin55@gmail.com>
2024-08-23 19:12:39 +0630

    Home page

--- src/App.js
+++ src/App.js
@@ -3,11 +3,8 @@ import './App.css'
 import Home from "./pages/home";
 
 const App = () => {
-  const name = "Mg Mg";
   return (
-    <div style={{fontWeight: "bold", fontSize: 30}} >
-      {name}
-    </div>
+    <Home />
   )
 }
 
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { FaArrowTrendUp } from "react-icons/fa6";
+import './home.css'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='allBorder'>
+      <TopNav />
+      <h1 className='title'>I create <span className='beautiful'>beautiful</span> <br /> experiences</h1>
+      <p className='text'>I work with people all over the world to create <br />tailor-made webflow experiences</p>
+      <p className='bottomText'>Available for freelance work</p>
+
+    </div>
   )
 }
+
+const TopNav = () => {
+  return (
+    <ul className='mainNav'>
+      <li><a href='#' className='name'>Hnin Nandar</a></li>
+      <li><a href='#'>LINKEDIN</a><FaArrowTrendUp className='Icon' /></li>
+      <li><a href='#'>TWITTER</a><FaArrowTrendUp className='Icon' /></li>
+      <li><a href='#'>INSTAGRAM</a><FaArrowTrendUp className='Icon' /></li>
+      <li className='talkBtn'><a href='#'>Let's TALK</a><FaArrowTrendUp className='Icon' /></li>
+    </ul>
+  )
+}
+
 
 export default Home;
